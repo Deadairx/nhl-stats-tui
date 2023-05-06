dc7079c49c52fb43b35cc2ab681fb3bc2da5d808
DeadAirX <djdivinity7@gmail.com>
2023-05-06 17:48:04 -0500

    First successful API call

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::env;
+
 use reqwest::Error;
-use serde::Deserialize;
 use tokio;
+use crate::players::{Players, Position};
+
+mod players;
 
 const SPORTS_DATA_IO_NHL_API_KEY: &str = "SPORTS_DATA_IO_NHL_KEY";
 
@@ -8,91 +12,34 @@ const SPORTS_DATA_IO_NHL_API_KEY: &str = "SPORTS_DATA_IO_NHL_KEY";
 async fn main() {
     let dallas_players = get_players_by_team("DAL".to_string()).await.unwrap();
 
-    println!("{:?}", dallas_players);
+    println!("Forwards");
+    println!("--------");
+    dallas_players
+        .iter()
+        .filter(|p| matches!(p.position, Position::Center | Position::LeftWing | Position::RightWing))
+        .for_each(|player| println!("{}, {} - {}", player.last_name, player.first_name, player.position));
+    println!("");
+    println!("Defense");
+    println!("--------");
+    dallas_players
+        .iter()
+        .filter(|p| matches!(p.position, Position::Defense))
+        .for_each(|player| println!("{}, {} - {}", player.last_name, player.first_name, player.position));
+    println!("");
+    println!("Goalies");
+    println!("--------");
+    dallas_players
+        .iter()
+        .filter(|p| matches!(p.position, Position::Goalie))
+        .for_each(|player| println!("{}, {} - {}", player.last_name, player.first_name, player.position));
 }
 
-async fn get_players_by_team(team_key: String) -> Result<Vec<Player>, Error> {
-    let api_endpoint = format!("https://api.sportsdata.io/v3/nhl/scores/json/Players/{}?key={}", team_key, SPORTS_DATA_IO_NHL_API_KEY);
+async fn get_players_by_team(team_key: String) -> Result<Players, Error> {
+    let api_endpoint = format!("https://api.sportsdata.io/v3/nhl/scores/json/Players/{}?key={}", team_key, env::var(SPORTS_DATA_IO_NHL_API_KEY).unwrap());
 
-    let response: Vec<Player> = reqwest::get(api_endpoint).await?.json().await?;
+    let response = reqwest::get(api_endpoint).await?;
+        
+    let players: Players = response.json().await?;
 
-    Ok(response)
-}
-
-#[derive(Debug, Deserialize)]
-struct Player {
-    player_id: String,
-    first_name: String,
-    last_name: String,
-    status: PlayerStatus,
-    team: Team,
-    position: PlayerPosition,
-    catches: Option<Hand>,
-    shoots: Option<Hand>,
-    /// in inches
-    height: usize,
-    /// in pounds
-    weight: usize,
-    photo_url: String,
-}
-
-#[derive(Debug, Deserialize)]
-enum PlayerPosition {
-    Goalie,
-    Center,
-    RightWing,
-    LeftWing,
-    Defense,
-}
-
-#[derive(Debug, Deserialize)]
-enum Hand {
-    Left,
-    Right,
-}
-
-#[derive(Debug, Deserialize)]
-enum PlayerStatus {
-    /// Is playing for the team
-    Active,
-    /// Player is on the associated Minor league team
-    Minor,
-    Inactive
-}
-
-#[derive(Debug, Deserialize)]
-enum Conference {
-    Western,
-    Eastern,
-}
-
-#[derive(Debug, Deserialize)]
-enum Division {
-    Atlantic,
-    Metropolitan,
-    Central,
-    Pacific,
-}
-
-#[derive(Debug, Deserialize)]
-struct TeamColors {
-    primary: String,
-    secondary: String,
-    tertiary: Option<String>,
-    quaternary: Option<String>,
-}
-
-#[derive(Debug, Deserialize)]
-struct Team {
-    team_id: usize,
-    team_key: String,
-    active: bool,
-    city: String,
-    name: String,
-    stadium_id: usize,
-    conference: Conference,
-    division: Division,
-    colors: TeamColors,
-    wiki_logo_url: String,
-    global_team_id: usize
+    Ok(players)
 }
---
+++ src/players.rs
@@ -0,0 +1,213 @@
+// Example code that deserializes and serializes the model.
+// extern crate serde;
+// #[macro_use]
+// extern crate serde_derive;
+// extern crate serde_json;
+//
+// use generated_module::Players;
+//
+// fn main() {
+//     let json = r#"{"answer": 42}"#;
+//     let model: Players = serde_json::from_str(&json).unwrap();
+// }
+
+use core::fmt;
+
+use serde::{Serialize, Deserialize};
+
+pub type Players = Vec<Player>;
+
+#[derive(Serialize, Deserialize)]
+pub struct Player {
+    #[serde(rename = "PlayerID")]
+    player_id: i64,
+
+    #[serde(rename = "FirstName")]
+    pub first_name: String,
+
+    #[serde(rename = "LastName")]
+    pub last_name: String,
+
+    #[serde(rename = "Status")]
+    status: Status,
+
+    #[serde(rename = "TeamID")]
+    team_id: i64,
+
+    #[serde(rename = "Team")]
+    team: Team,
+
+    #[serde(rename = "Position")]
+    pub position: Position,
+
+    #[serde(rename = "Jersey")]
+    jersey: Option<i64>,
+
+    #[serde(rename = "Catches")]
+    catches: Option<Catches>,
+
+    #[serde(rename = "Shoots")]
+    shoots: Option<Catches>,
+
+    #[serde(rename = "Height")]
+    height: i64,
+
+    #[serde(rename = "Weight")]
+    weight: i64,
+
+    #[serde(rename = "BirthDate")]
+    birth_date: String,
+
+    #[serde(rename = "BirthCity")]
+    birth_city: String,
+
+    #[serde(rename = "BirthState")]
+    birth_state: Option<String>,
+
+    #[serde(rename = "PhotoUrl")]
+    photo_url: String,
+
+    #[serde(rename = "SportRadarPlayerID")]
+    sport_radar_player_id: String,
+
+    #[serde(rename = "RotoworldPlayerID")]
+    rotoworld_player_id: Option<i64>,
+
+    #[serde(rename = "RotoWirePlayerID")]
+    roto_wire_player_id: Option<i64>,
+
+    #[serde(rename = "FantasyAlarmPlayerID")]
+    fantasy_alarm_player_id: Option<i64>,
+
+    #[serde(rename = "StatsPlayerID")]
+    stats_player_id: Option<i64>,
+
+    #[serde(rename = "SportsDirectPlayerID")]
+    sports_direct_player_id: Option<i64>,
+
+    #[serde(rename = "XmlTeamPlayerID")]
+    xml_team_player_id: Option<i64>,
+
+    #[serde(rename = "InjuryStatus")]
+    injury_status: Injury,
+
+    #[serde(rename = "InjuryBodyPart")]
+    injury_body_part: Injury,
+
+    #[serde(rename = "InjuryStartDate")]
+    injury_start_date: Option<serde_json::Value>,
+
+    #[serde(rename = "InjuryNotes")]
+    injury_notes: Injury,
+
+    #[serde(rename = "FanDuelPlayerID")]
+    fan_duel_player_id: Option<i64>,
+
+    #[serde(rename = "DraftKingsPlayerID")]
+    draft_kings_player_id: Option<i64>,
+
+    #[serde(rename = "YahooPlayerID")]
+    yahoo_player_id: Option<i64>,
+
+    #[serde(rename = "FanDuelName")]
+    fan_duel_name: Option<String>,
+
+    #[serde(rename = "DraftKingsName")]
+    draft_kings_name: Option<String>,
+
+    #[serde(rename = "YahooName")]
+    yahoo_name: Option<String>,
+
+    #[serde(rename = "DepthChartPosition")]
+    depth_chart_position: Option<Position>,
+
+    #[serde(rename = "DepthChartOrder")]
+    depth_chart_order: Option<i64>,
+
+    #[serde(rename = "GlobalTeamID")]
+    global_team_id: i64,
+
+    #[serde(rename = "FantasyDraftName")]
+    fantasy_draft_name: Option<String>,
+
+    #[serde(rename = "FantasyDraftPlayerID")]
+    fantasy_draft_player_id: Option<i64>,
+
+    #[serde(rename = "UsaTodayPlayerID")]
+    usa_today_player_id: Option<i64>,
+
+    #[serde(rename = "UsaTodayHeadshotUrl")]
+    usa_today_headshot_url: Option<String>,
+
+    #[serde(rename = "UsaTodayHeadshotNoBackgroundUrl")]
+    usa_today_headshot_no_background_url: Option<String>,
+
+    #[serde(rename = "UsaTodayHeadshotUpdated")]
+    usa_today_headshot_updated: Option<String>,
+
+    #[serde(rename = "UsaTodayHeadshotNoBackgroundUpdated")]
+    usa_today_headshot_no_background_updated: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Catches {
+    #[serde(rename = "-")]
+    Empty,
+
+    #[serde(rename = "L")]
+    Left,
+
+    #[serde(rename = "R")]
+    Right,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Position {
+    #[serde(rename = "C")]
+    Center,
+
+    #[serde(rename = "D")]
+    Defense,
+
+    #[serde(rename = "G")]
+    Goalie,
+
+    #[serde(rename = "LW")]
+    LeftWing,
+
+    #[serde(rename = "RW")]
+    RightWing,
+}
+
+impl fmt::Display for Position {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Position::Center => write!(f, "Center"),
+            Position::Defense => write!(f, "Defense"),
+            Position::Goalie => write!(f, "Goalie"),
+            Position::LeftWing => write!(f, "LeftWing"),
+            Position::RightWing => write!(f, "RightWing"),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Injury {
+    #[serde(rename = "Scrambled")]
+    Scrambled,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Status {
+    #[serde(rename = "Active")]
+    Active,
+
+    #[serde(rename = "Minors")]
+    Minors,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Team {
+    #[serde(rename = "DAL")]
+    Dal,
+}
